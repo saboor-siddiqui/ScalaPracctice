361b86c9ec3a4cd9d4c06c5b51fab30135444ab1
Saboor Siddiqui <saboorsiddiqui0@gmail.com>
2022-10-09 22:18:03 +0530

    Adding some more programs

--- src/main/scala/DataFrames_API_Study/UDFExample.scala
+++ src/main/scala/DataFrames_API_Study/UDFExample.scala
@@ -11,16 +11,18 @@ object UDFExample extends App {
     val sc = spark.sparkContext
     import spark.implicits._
     import spark.sql
+
+  case class Person(name: String ,age: Int ,city: String)
     var df = spark.read
       .format("csv")
-      .option("header","true")
       .option("inferSchema","true")
       .option("path","C:/Users/sabbi/OneDrive/Desktop/Shared Folder//-201025-223502.dataset1")
       .load()
 
-    df.printSchema()
-    df.show(false)
-
+    val df_1: Dataset[Row] = df.toDF("name","age","city")
+    val ds_1 = df_1.as[Person]
+    ds_1.printSchema()
+    ds_1.show(false)
     scala.io.StdIn.readLine()
     spark.stop()
 }
