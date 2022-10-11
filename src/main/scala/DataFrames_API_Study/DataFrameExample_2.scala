3b17343b3f8cda74c0cb1cd0ea393281f17d9299
Saboor Siddiqui <saboorsiddiqui0@gmail.com>
2022-10-11 22:41:42 +0530

    Adding some more programs

---
+++ src/main/scala/DataFrames_API_Study/DataFrameExample_2.scala
@@ -0,0 +1,29 @@
+package org.saboor.scalapractice
+package DataFrames_API_Study
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.DateType
+
+object DataFrameExample_2 extends App {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val spark = SparkSession.builder.master("local[*]").appName("DataFrameExample_2").getOrCreate()
+    val sc = spark.sparkContext
+    import spark.implicits._
+    import spark.sql
+  val myList = List((1,"2013-07-25",11599,"CLOSED"),
+    (2,"2013-07-25",256,"PENDING_PAYMENT"),
+    (3,"2013-07-25",11599,"COMPLETE"),
+    (4,"2013-07-25",8827,"CLOSED"))
+
+  var df1 = spark.createDataFrame(myList) .toDF("order_id","order_date","customer_id","order_status")
+  //spark.sparkContext.parallelize(myList).toDF() - Method Involving RDD
+  var df_new = df1.withColumn("order_date",unix_timestamp(col("order_date").cast(DateType)))
+  df_new.printSchema()
+  df_new.show(false)
+
+    scala.io.StdIn.readLine()
+    spark.stop()
+
+}
