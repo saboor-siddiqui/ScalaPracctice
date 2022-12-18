5a171c83066e30a135728e085eb79fce4c2161a2
Saboor Siddiqui <saboorsiddiqui0@gmail.com>
2022-12-18 22:20:00 +0530

    Learning Series for Big Data

--- src/main/scala/DataFrames_API_Study/AggregationExample.scala
+++ src/main/scala/DataFrames_API_Study/AggregationExample.scala
@@ -19,7 +19,17 @@ object AggregationExample extends App{
     .option("path","C:/Users/sabbi/OneDrive/Desktop/Shared Folder//order_data-201025-223502.csv")
     .load()
 
-   //invoice_df.select(count(*).as("row_count"))
+//   invoice_df.select(
+//     count("*").as("RowCount"),
+//     sum("Quantity").as("TotalQuantity"),
+//     avg("UnitPrice").as("AvgPrice")
+//   ).show()
+
+  invoice_df.selectExpr(
+    "count(StockCode) as RowCount",
+    "sum(Quantity) as TotalQuantity",
+    "avg(UnitPrice) as AvgPrice "
+    ).show(2,false)
 
     scala.io.StdIn.readLine()
     spark.stop()
